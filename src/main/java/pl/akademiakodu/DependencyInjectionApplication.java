7c2a05a5a873a6c17b02d362b6cb609216d7bbfb
mwierba <michal.wierba@rkantor.com>
2020-12-06 08:46:55 +0100

    second commit

--- src/main/java/pl/akademiakodu/DependencyInjectionApplication.java
+++ src/main/java/pl/akademiakodu/DependencyInjectionApplication.java
@@ -1,22 +1,12 @@
 package pl.akademiakodu;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ImportResource;
-import pl.akademiakodu.beans.MessagePrinter;
 
 @SpringBootApplication
-@ImportResource("classpath:beans-config.xml")
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
-		MessagePrinter printer = ctx.getBean(MessagePrinter.class);
-		System.out.println("DZIAŁA");
-		printer.print();
-		System.out.println("Haha:) "+printer.getName());
-		ctx.close();
+
 
 	}
 }
